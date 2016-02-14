4c52768b2313579db5d14230c36113718d00835f
mlvlasic <matthewvlasic@Matthews-MacBook-Pro-3.local>
2016-02-13 21:59:37 -0500

    getting there

--- studybar.js
+++ studybar.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
 
 var studyBarImages = [
-	
-]
+	"coffeebar.jpg",
+	"computer.jpg"
+];
+
+var currentImage = 0;
+
+$('.nextButton').click(function() {
+	currentImage++
+	if (currentImage == studyBarImages.length) {
+		currentImage = 0
+	}
+
+	$('#coffee').attr('src', studyBarImages[currentImage]);
+
+});
+
+$('.backButton').click(function() {
+	currentImage--
+	if (currentImage < 0) {
+		currentImage = studyBarImages.length - 1
+	}
+
+	$('#coffee').attr('src', studyBarImages[currentImage]);
 
 
+});
 
 
 
